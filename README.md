67b96245aa04ef10bbe16813f615742a392f267d
TomGitt <redgrom@ukr.net>
2023-05-13 04:53:27 +0300

    temporary3 :wq q :wq
    :wq
    /wq

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Коваленко Сергей
 13:30 03.05.2023
+
+Command-option-L - formating
+Command-D - duplicate
+
+
---
+++ strings.py
@@ -0,0 +1,17 @@
+my_love = 'I love Python'
+city = 'Odesa'
+
+number_one = 5
+number_two = 4
+n = number_two
+
+print(id(number_two))
+
+
+result = number_two * 10 + number_one
+print(number_one, number_two, sep='^^^', end='\n', )
+print(result)
+
+
+
+
