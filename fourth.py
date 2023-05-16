14b8ca75304796d2fef1b045a805e8449df5584d
TomGitt <redgrom@ukr.net>
2023-05-16 06:04:04 +0300

    fourth_lesson

---
+++ fourth.py
@@ -0,0 +1,6 @@
+input_name = input("Введіть імя ").title().strip(' 0123456789')
+input_second_name = input("Введіть прізвище ").upper().strip(' 0123456789')
+quantity = len(input_name)
+template = f'Привіт,  {input_name}  {input_second_name}  а ти знав, що твоє імя складється з {quantity} літер'
+
+print(template)
