896432c97ec732966b40c1dffd20a5bbfee03876
TomGitt <redgrom@ukr.net>
2023-05-18 16:36:46 +0300

    better

--- dxsix.py
+++ dxsix.py
@@ -4,9 +4,9 @@ import math
 
 pi = math.pi
 
-INPUT_RADIUS = float(input('Введіть радіус кулі>>>'))
-radius = INPUT_RADIUS ** 3
-ball_volume = 4 / 3 * pi * radius
+input_radius = float(input('Введіть радіус кулі>>>'))
+radius_cubed = input_radius ** 3
+ball_volume = 4 / 3 * pi * radius_cubed
 ball_volume_final = Decimal(str(ball_volume)).quantize(Decimal('0.01'))
 
 print(ball_volume_final)
