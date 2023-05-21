92de2a58fd6b875a2b9ba42830baff1072a7ac0e
TomGitt <redgrom@ukr.net>
2023-05-21 10:40:32 +0300

    8les2

--- lesson8.py
+++ lesson8.py
@@ -1,1 +1,21 @@
-cv
+enter_login = str(input('Enter login '))
+
+true = True
+false = False
+
+if enter_login  enter_login.title():
+    print('correct')
+# elif enter_login == 1000:
+#     print('ten')
+# elif enter_login <999:
+#     print('easy999')
+
+
+
+
+
+
+
+
+
+
