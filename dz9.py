4b64a3e35a824893c5bf8f9c428a3feffee86e97
TomGitt <redgrom@ukr.net>
2023-05-24 19:18:19 +0300

    dz9hz

--- dz9.py
+++ dz9.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+random_list = ['re', 'fd', 'eee', 'dvdvdd', 'popopopo']
+# rtr = [43]
+random_list.append('https://soundtracks.pro/movie/oblasti-tmy')
+# print(len(random_list))
+# print(add_list)
+random_list.sort()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# print(rtr)
+# print(random_list)
+# print(sorty_list)
+
+# tr = 'sd dfd df  dfd'
+# d = [tr]
+# o = [random_list]
+# print(tr.split())
+# print(d)
+# print(o)
+
+for el in random_list:
+    print(el)
+# print(random_list.index(9))
+# print(random_list.index(6.6))
+# print(random_list.index(44))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
