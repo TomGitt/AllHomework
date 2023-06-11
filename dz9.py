75b770aeb222111e2878ebf6b4dde1e756cf39bf
TomGitt <redgrom@ukr.net>
2023-06-11 11:32:16 +0300

    dfdf

---
+++ dz9.py
@@ -0,0 +1,9 @@
+new_list = [7, 4.7, 1, 1.1, -2]
+multiplication_list = []
+
+for number in new_list:
+    doubled_number = number * 2
+    string_number = str(doubled_number)
+    multiplication_list.append(string_number)
+
+print(multiplication_list)
