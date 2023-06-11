0e6526e6c52ed536c741de83e38e0200ce5a9582
TomGitt <redgrom@ukr.net>
2023-06-11 10:56:23 +0300

    hnya2

--- dz9.py
+++
@@ -1,9 +0,0 @@
-new_list = [7, 4.7, 1, 1.1, -2]
-multiplication_list = []
-
-for number in new_list:
-    doubled_number = number * 2
-    string_number = str(doubled_number)
-    multiplication_list.append(string_number)
-
-print(multiplication_list)
--- dz9_2.py
+++
@@ -1,9 +0,0 @@
-list_student = input('Введіть призвищя учнів ').title()
-
-list_student_split = list_student.split()
-list_student_split = [name for name in list_student_split if
-                      name.isalpha()]  # виключеня числових значеннь чесно взяв у гпт:)
-list_student_split.sort()
-
-for list_final in list_student_split:
-    print(list_final)
